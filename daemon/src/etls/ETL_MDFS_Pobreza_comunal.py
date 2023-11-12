4a765aab767a8c9545b225d960ebb723782ef5da
JJhano <jhanobk@gmail.com>
2023-11-12 03:27:08 -0300

    ETL_MDFS_pobreza lista

--- daemon/src/etls/ETL_MDFS_Pobreza_comunal.py
+++ daemon/src/etls/ETL_MDFS_Pobreza_comunal.py
@@ -33,8 +33,6 @@ class ETL_Transactional:
         self.extractedData.columns = self.extractedData.iloc[1]
         self.extractedData = self.extractedData[2:346].reset_index()
         self.extractedData = self.extractedData[["Código","Nombre comuna", "Número de personas en situación de pobreza por ingresos (**)", "Porcentaje de personas en situación de pobreza por ingresos 2020"]]
-        print(self.extractedData)
-        print(self.extractedData[["Número de personas en situación de pobreza por ingresos (**)", "Nombre comuna"]])
         for _, comuna in comunas.iterrows():
             comunaData = self.extractedData[(self.extractedData['Código'] == comuna['comuna_id'])]
             for _, row in comunaData.iterrows():
@@ -54,7 +52,6 @@ class ETL_Transactional:
                     "dimension_id": getDimension(self.dimension)              
                     }
                 dataToLoad.append(data)
-                # print(data)
         return(dataToLoad)
     
     def Load(self, data):
@@ -81,84 +78,84 @@ class ETL_Transactional:
 ## -------------------------------------- ##
 ## -------------------------------------- ##
 
-# class ETL_Processing:
-#     def __init__(self, querys, localidades):
-#         # Para la base de datos
-#         self.fuente = "MDFS_Pobreza_Comunal"              
-#         self.nombreIndicador = "Minvu_Areas_Verdes"
+class ETL_Processing:
+    def __init__(self, querys, localidades):
+        # Para la base de datos
+        self.fuente = "MDFS_Pobreza_Comunal"              
+        self.nombreIndicador = "MDFS_Pobreza_Comunal"
         
-#         # informacion indicador
-#         self.indicador_id = 11  ## Valor numerico, revisar si no existe en bd
-#         self.dimension = "Ecologico"
-#         self.prioridad = 1
-#         self.url =  "https://observatorio.ministeriodesarrollosocial.gob.cl/pobreza-comunal-2020"
-#         self.descripcion = "Indicador asociado a la suma de las superficies de los parques urbanos por ciudad"
+        # informacion indicador
+        self.indicador_id = 13  ## Valor numerico, revisar si no existe en bd
+        self.dimension = "Economico"
+        self.prioridad = 1
+        self.url =  "https://observatorio.ministeriodesarrollosocial.gob.cl/pobreza-comunal-2020"
+        self.descripcion = "Indicador asociado a la pobreza comunal"
         
-#         # << No modificar >>
-#         self.tableName = "data_" + self.fuente
-#         self.dimension = getDimension(self.dimension)
-#         self.localidades = localidades
-#         self.transaccionalData = None
-#         self.querys = querys
-#         # << No modificar >>  
+        # << No modificar >>
+        self.tableName = "data_" + self.fuente
+        self.dimension = getDimension(self.dimension)
+        self.localidades = localidades
+        self.transaccionalData = None
+        self.querys = querys
+        # << No modificar >>  
 
-#     def __string__(self):
-#         return str(self.nombreIndicador)
+    def __string__(self):
+        return str(self.nombreIndicador)
 
-#     def Extract(self):
-#         self.transaccionalData = self.querys.getTransactionalData(self.tableName)
+    def Extract(self):
+        self.transaccionalData = self.querys.getTransactionalData(self.tableName)
         
-#     def Transform(self, comuna):
-#         #Revisar
-#         df = self.transaccionalData
-#         df_merged = df.merge(comuna, left_on='comuna_id', right_on='comuna_id', how='right')
-#         df_merged = df_merged[['comuna_id','dimension_id','superficie']]
-#         sup_total_comunas =  df_merged.groupby('comuna_id')['superficie'].sum().reset_index().drop_duplicates(subset="comuna_id")
-#         df_merged = df_merged.drop_duplicates(subset="comuna_id").reset_index()
-#         df_merged["valor"] = sup_total_comunas['superficie'] / comuna['poblacion']
-#         data = df_merged[['comuna_id', 'valor', 'dimension_id']]
-#         data['valor'] = data['valor'].fillna(0)
-#         normalized = dataNormalize(data)
-#         return normalized
+    def Transform(self, comuna):
+        #Revisar
+        df = self.transaccionalData
+        df_merged = df.merge(comuna, left_on='comuna_id', right_on='comuna_id', how='right')
+        df_merged = df_merged[['comuna_id','dimension_id','numero_personas_pobreza']]
+        num_personas_pobreza =  df_merged.groupby('comuna_id')['numero_personas_pobreza'].sum().reset_index().drop_duplicates(subset="comuna_id")
+        df_merged = df_merged.drop_duplicates(subset="comuna_id").reset_index()
+        df_merged["valor"] = num_personas_pobreza['numero_personas_pobreza'] / comuna['poblacion']
+        data = df_merged[['comuna_id', 'valor', 'dimension_id']]
+        data['valor'] = data['valor'].fillna(0)
+        normalized = dataNormalize(data)
+        return normalized
  
-#     def Load(self, data):    
-#         all_data = [] 
-#         for _, values in data.iterrows():
-#             valor = values['valor']
-#             comuna_id = values['comuna_id']
-#             if pd.isnull(valor):
-#                 valor = 0
-#             data = {
-#                 "valor": valor,
-#                 "fecha" : datetime.now().date(),
-#                 "flag" : True,
-#                 "dimension_id" : self.dimension,
-#                 "comuna_id" : comuna_id,
-#                 "indicador_id": self.indicador_id
-#             }
-#             all_data.append(data) 
-#         self.querys.loadDataProcessing(all_data)
+    def Load(self, data):    
+        all_data = [] 
+        for _, values in data.iterrows():
+            valor = values['valor']
+            comuna_id = values['comuna_id']
+            if pd.isnull(valor):
+                valor = 0
+            data = {
+                "valor": valor,
+                "fecha" : datetime.now().date(),
+                "flag" : True,
+                "dimension_id" : self.dimension,
+                "comuna_id" : comuna_id,
+                "indicador_id": self.indicador_id
+            }
+            all_data.append(data) 
+        self.querys.loadDataProcessing(all_data)
         
-#     def addETLinfo(self):
-#         data = {
-#             "indicadoresinfo_id": self.indicador_id,
-#             "nombre": self.nombreIndicador,
-#             "prioridad": self.prioridad,
-#             "descripcion": self.descripcion,
-#             "fuente": self.url,
-#             "dimension": self.dimension
-#         }
-#         self.querys.addIndicatorsInfo(data)
+    def addETLinfo(self):
+        data = {
+            "indicadoresinfo_id": self.indicador_id,
+            "nombre": self.nombreIndicador,
+            "prioridad": self.prioridad,
+            "descripcion": self.descripcion,
+            "fuente": self.url,
+            "dimension": self.dimension
+        }
+        self.querys.addIndicatorsInfo(data)
 
-#     def ETLProcess(self):
-#         try:
-#             self.addETLinfo()
-#             self.Extract()
-#             self.querys.updateFlagProcessing(self.indicador_id)
-#             comunas = self.localidades.getDataComunas()
-#             data = self.Transform(comunas)
-#             self.Load(data)
+    def ETLProcess(self):
+        try:
+            self.addETLinfo()
+            self.Extract()
+            self.querys.updateFlagProcessing(self.indicador_id)
+            comunas = self.localidades.getDataComunas()
+            data = self.Transform(comunas)
+            self.Load(data)
 
-#         except Exception as error:
-#             print(error)
-#         return {"OK": 200, "mesagge": "Indicators is updated successfully"}
+        except Exception as error:
+            print(error)
+        return {"OK": 200, "mesagge": "Indicators is updated successfully"}
